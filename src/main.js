9529b9134b46f553357a80bda30e8cefb5cbac73
hridaykrishna2007-a11y <hridaykrishna2007@gmail.com>
2026-07-17 10:53:38 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 const API_KEY = import.meta.env.VITE_NASA_API_KEY;
 
-document.querySelector("#APP").innerHTML = "<p>loading...</p>";
+document.querySelector("#app").innerHTML = "<p>loading...</p>";
 
 fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`).
 then ( response => response.json()).then (data => {
